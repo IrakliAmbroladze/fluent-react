7b424f5b9c01715338b318ed104a77138e6c453d
Irakli <ambroladzeirakli@gmail.com>
2025-04-21 17:34:29 +0400

    feat(like): add html like button

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const likeButton = document.getElementById("likeButton");
 likeButton.addEventListener("click", () => {
-  console.log("button clicked");
+  const liked = likeButton.getAttribute("data-liked") === "true";
+  const isPending = likeButton.getAttribute("data-pending") === "true";
+
+  likeButton.setAttribute("data-pending", "true");
+  likeButton.setAttribute("disabled", "true");
+
+  fetch("/like", {
+    method: "POST",
+    body: JSON.stringify({ liked: !liked }),
+  })
+    .then(() => {
+      likeButton.setAttribute("data-liked", !liked);
+      likeButton.textContent = liked ? "Like" : "Liked";
+      likeButton.removeAttribute("disabled");
+    })
+    .catch(() => {
+      likeButton.setAttribute("data-failed", "true");
+      likeButton.textContent = "Failed";
+    })
+    .finally(() => {
+      likeButton.setAttribute("data-pending", "false");
+    });
 });
